311c92ab1abc109f8274eb4d4c115dad1dd28968
Lena <elena.mikhaylova0@gmail.com>
2025-04-25 13:50:51 -0400

    add additional check

--- scripts/fetch-vaults.js
+++ scripts/fetch-vaults.js
@@ -5,10 +5,10 @@ const https = require("https");
 const VAULT_CONFIG_URL = process.env.VAULT_CONFIG_URL;
 const isProduction = process.env.NODE_ENV === "production";
 
-function fetchVaults() {
+function fetchVaults(url) {
   return new Promise((resolve, reject) => {
     https
-      .get(VAULT_CONFIG_URL, (res) => {
+      .get(url, (res) => {
         let data = "";
 
         res.on("data", (chunk) => {
@@ -38,8 +38,13 @@ async function main() {
         "Not fetching vaults in non-production environment. Please modify vaults.config.  json in the root folder.",
       );
       return;
+    } else if (!VAULT_CONFIG_URL) {
+      console.log(
+        "VAULT_CONFIG_URL is not set. Please set it in the .env file.",
+      );
+      return;
     }
-    const data = await fetchVaults();
+    const data = await fetchVaults(VAULT_CONFIG_URL);
     const outputPath = path.join(process.cwd(), "vaults.config.json");
     fs.writeFileSync(outputPath, data);
     console.log("Successfully fetched and saved vaults.config.json");
